89ab916692cc8dc497d5b5bc8ed1512b36e4e35c
liulin <liulin1987315@gmail.com>
2017-06-06 19:43:38 +1000

    Use CommonsChunkPlugin to chunk different files.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
+var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-	entry: './app/index.js',
+	entry: {
+		main: './app/index.js',
+		vendor: ['moment', 'lodash'],
+		admin: './app/admin.js'
+	},
 	output: {
-		filename: 'bundle.js',
+		filename: '[name].[chunkhash].js',
 		path: path.resolve(__dirname, 'dist')
-	}
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'common_vendor',
+			chunks: ['main', 'vendor']
+		}),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'node_module_vendor',
+			// this assumes your vendor imports exist in the node_modules directory
+			minChunks: function(module) {
+				return module.context && module.context.indexOf('node_modules') !== -1;
+			}
+		})
+	]
 };
