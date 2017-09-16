78fb555c9fa4c3500f5b15cb6650f3f73f379763
liulin <liulin1987315@gmail.com>
2017-09-16 11:53:14 +1000

    Reformat.

--- src/js/components/__tests__/App.test.js
+++ src/js/components/__tests__/App.test.js
@@ -4,14 +4,14 @@ import NameInput from '../NameInput';
 import App from '../App';
 import userStore from '../../stores/userStore';
 
-describe(('App'), () => {
-  it(('should pass props'), () => {
-    const wrapper = shallow(<App/>);
+describe('App', () => {
+  it('should pass props', () => {
+    const wrapper = shallow(<App />);
     expect(wrapper.props().userStore).toBe(userStore);
   });
 
-  it(('should render NameInput'), () => {
-    const wrapper = shallow(<App/>);
+  it('should render NameInput', () => {
+    const wrapper = shallow(<App />);
     expect(wrapper.find(NameInput).length).toBe(1);
   });
 });
